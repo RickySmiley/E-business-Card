c44e146bb403ac19dbc63c2a65ab5f902bc3289c
RickySmiley <zermenor29@gmail.com>
2024-04-12 08:38:18 -0500

    added tentative working version of calling card

---
+++ jrickyze/cli.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+
+import boxen from "boxen";
+import chalk from "chalk";
+
+const boxenOptions = {
+	padding: 2,
+	width: 85,
+	title: "jrickyze",
+	titleAlignment: "center",
+	borderStyle: "double",
+	borderColor: "#b906f3",
+};
+
+const intro = chalk.bold("Jose Zermeno |" + chalk.hex("#ff0eb2") (" Software Developer ") + "\nFind me on the internet.\n\n");
+
+
+const links = [
+	{ name: chalk.hex("#ffffff").bgHex("#f30909")(" NPM "), urls:["        https://www.npmjs.com/~zermenor29"] },
+	{ name: chalk.hex("#0e0606").bgHex("#ffffff")(" GitHub "), urls:  ["  https://www.linkedin.com/in/jose-zermeno-778965b9/"]},
+	{ name: chalk.hex("#fff").bgHex("#1829e5")(" LinkedIn "), urls: ["    https://github.com/RickySmiley"]},
+];
+
+const linkList = links.map((link) => `${link.name} ${link.urls.join('\n')}`).join("\n");
+
+
+console.log(boxen(intro + linkList, boxenOptions));
---
+++ jrickyze/index.js
@@ -0,0 +1,12 @@
+#!./index.js
+
+const { render } = require('prettyjson')
+const data = require('./data')
+
+const renderOpts = {
+	dashColor: 'cyan',
+	keysColor: 'blue',
+	stringColor: 'white'
+}
+
+module.exports = ({ json }) => json ? JSON.stringify(data) : render(data, renderOpts)
